0a3f233f1b0f1f5b1348c75983a1e52f9f7fcd15
Chihakuro <92619401+Chihakuro@users.noreply.github.com>
2022-10-04 08:36:08 +0700

    Create py1.py

---
+++ py1.py
@@ -0,0 +1,11 @@
+#Initialization
+class xd:
+  def __init__(self, name):
+    self.name = name
+    
+  def hello(self):
+    print('Welcome, the chosen one from the bottom of society!/n')
+    n = input('Let me know your sacred name: ')
+    print('Nice to meet you again',self.name,'. This cringy kingdom is for you! Glhf'))
+    
+hello('Khoa gei')
