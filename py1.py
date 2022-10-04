58c43a5681a012a5e427bc78fd329ad442984c2e
Chihakuro <92619401+Chihakuro@users.noreply.github.com>
2022-10-04 08:43:07 +0700

    Update py1.py

--- py1.py
+++ py1.py
@@ -4,8 +4,9 @@ class xd:
     self.name = name
     
   def hello(self):
-    print('Welcome, the chosen one from the bottom of society!/n')
-    n = input('Let me know your sacred name: ')
     print('Nice to meet you again',self.name,'. This cringy kingdom is for you! Glhf')
-    
-hello('Khoa gei')
+
+print('Welcome, the chosen one from the bottom of society!')
+n = input('Let me know your sacred name: ')
+v = xd(n)
+v.hello()
